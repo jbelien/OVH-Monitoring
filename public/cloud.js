9067cec90a177a5faba46e5a4aecf22cfb546e4d
Jonathan Beliën <jbe@geo6.be>
2018-02-08 14:02:16 +0100

    Update cloud.js

--- public/cloud.js
+++ public/cloud.js
@@ -3,6 +3,121 @@
 var chart = null;
 var consoleId = 1;
 
+
+/**
+ *
+ */
+ function callBackAlert() {
+   $.getJSON("alert-xhr.php", {
+     "cloud": 1,
+     "time": Date.now()
+   }, function (json) {
+     $("tr[data-instance]").each(function () {
+       var project = $(this).data("project");
+       var instance = $(this).data("instance");
+       var td = $(this).find("td.alert-live");
+
+       if (typeof json[project] !== "undefined" && typeof json[project][instance] !== "undefined") {
+         var alerts = json[project][instance].alerts || [];
+         var status = json[project][instance].status;
+
+         $(this).data("alerts", alerts);
+
+         var badge = null;
+         switch (status) {
+           case "planned":
+             badge = "badge-warning";
+             break;
+           case "inProgress":
+             badge = "badge-danger";
+             break;
+           case "finished":
+             badge = "badge-info";
+             break;
+           default:
+             badge = "badge-default";
+             break;
+         }
+
+         if (alerts.length > 0) {
+           $(td).html("<a href=\"#modal-alert\" data-toggle=\"modal\"><span class=\"badge badge-pill " + badge + "\">" + alerts.length + "</span></a>");
+         } else {
+           $(td).empty();
+         }
+       } else {
+         $(td).empty();
+       }
+     });
+   });
+ }
+
+ /**
+ *
+ */
+function callBackCPU() {
+  $.getJSON("cloud-xhr.php", {
+    "cpu": 1,
+    "time": Date.now()
+  }, function(json) {
+    $("tr[data-instance]").each(function() {
+      var project = $(this).data("project");
+      var instance = $(this).data("instance");
+      var td = $(this).find("td.cpu-live");
+
+      if (typeof json[project][instance] === "object") {
+        $(td).html("<span title=\"" + Math.round(json[project][instance][0]) + " " + json[project][instance][1] + "\" class=\"" + (json[project][instance][2] < 50 ? "text-success" : (json[project][instance][2] < 75 ? "text-warning" : "text-danger")) + "\">" + json[project][instance][2] + "%</span>");
+        if (json[project][instance][3] === -1) {
+          $(td).append(" <i class=\"fa fa-angle-double-down fa-fw\" aria-hidden=\"true\"></i>");
+        } else if (json[project][instance][3] === 1) {
+          $(td).append(" <i class=\"fa fa-angle-double-up fa-fw\" aria-hidden=\"true\"></i>");
+        } else {
+          $(td).append(" <i class=\"fa fa-circle-thin fa-fw\" aria-hidden=\"true\" style=\"visibility: hidden;\"></i>");
+        }
+      } else {
+        $(td).html("<span class=\"text-muted\">N/A</span><a href=\"#console-" + consoleId + "\"><sup>" + consoleId + "</sup></a>");
+
+        $("#console > ol").append("<li id=\"console-" + consoleId + "\"><samp>" + json[project][instance] + "</samp></li>");
+        consoleId++;
+      }
+    });
+  });
+}
+
+/**
+ *
+ */
+function callBackRAM() {
+  $.getJSON("cloud-xhr.php", {
+    "ram": 1,
+    "time": Date.now()
+  }, function(json) {
+    $("tr[data-instance]").each(function() {
+      var project = $(this).data("project");
+      var instance = $(this).data("instance");
+      var td = $(this).find("td.ram-live");
+
+      if (typeof json[project][instance] === "object") {
+        $(td).html("<span title=\"" + Math.round(json[project][instance][0]) + " " + json[project][instance][1] + "\" class=\"" + (json[project][instance][2] < 50 ? "text-success" : (json[project][instance][2] < 75 ? "text-warning" : "text-danger")) + "\">" + json[project][instance][2] + "%</span>");
+        if (json[project][instance][3] === -1) {
+          $(td).append(" <i class=\"fa fa-angle-double-down fa-fw\" aria-hidden=\"true\"></i>");
+        } else if (json[project][instance][3] === 1) {
+          $(td).append(" <i class=\"fa fa-angle-double-up fa-fw\" aria-hidden=\"true\"></i>");
+        } else {
+          $(td).append(" <i class=\"fa fa-circle-thin fa-fw\" aria-hidden=\"true\" style=\"visibility: hidden;\"></i>");
+        }
+      } else {
+        $(td).html("<span class=\"text-muted\">N/A</span><a href=\"#console-" + consoleId + "\"><sup>" + consoleId + "</sup></a>");
+
+        $("#console > ol").append("<li id=\"console-" + consoleId + "\"><samp>" + json[project][instance] + "</samp></li>");
+        consoleId++;
+      }
+    });
+  });
+}
+
+/**
+ *
+ */
 $(document).ready(function() {
   /*var intervalAlert = window.setInterval(callBackAlert, (5*60)*1000);*/ callBackAlert();
   /*var intervalCPU = window.setInterval(callBackCPU, (5*60)*1000);*/ callBackCPU();
@@ -13,8 +128,8 @@ $(document).ready(function() {
     var instance = $(event.relatedTarget).closest("tr").data("instance");
     var params = {
       "info": 1,
-      "instance": instance,
-      "project": project,
+      instance,
+      project,
       "time": Date.now()
     };
 
@@ -32,12 +147,12 @@ $(document).ready(function() {
     for (var i = 0; i < alerts.length; i++) {
       var row = document.createElement("tr");
 
-      $(row).append("<td>" + new Date(alerts[i].startDate).toString() + "</td>")
-      $(row).append("<td>" + alerts[i].reference + "</td>")
-      $(row).append("<td><strong>" + alerts[i].title + "</strong><br>" + alerts[i].details + "</td>")
-      $(row).append("<td>" + alerts[i].status + (alerts[i].status === "inProgress" ? " (" + alerts[i].progress + "%)" : "") + "</td>")
-      $(row).append("<td>" + alerts[i].type + "</td>")
-      $(row).append("<td>" + alerts[i].impact + "</td>")
+      $(row).append("<td>" + new Date(alerts[i].startDate).toString() + "</td>");
+      $(row).append("<td>" + alerts[i].reference + "</td>");
+      $(row).append("<td><strong>" + alerts[i].title + "</strong><br>" + alerts[i].details + "</td>");
+      $(row).append("<td>" + alerts[i].status + (alerts[i].status === "inProgress" ? " (" + alerts[i].progress + "%)" : "") + "</td>");
+      $(row).append("<td>" + alerts[i].type + "</td>");
+      $(row).append("<td>" + alerts[i].impact + "</td>");
 
       $("#modal-alert .modal-body > table > tbody").append(row);
     }
@@ -123,114 +238,3 @@ $(document).ready(function() {
     });
   });
 });
-
-/* ************************************************************************
- *
- */
- function callBackAlert() {
-   $.getJSON("alert-xhr.php", {
-     "cloud": 1,
-     "time": Date.now()
-   }, function (json) {
-     $("tr[data-instance]").each(function () {
-       var project = $(this).data("project");
-       var instance = $(this).data("instance");
-       var td = $(this).find("td.alert-live");
-
-       if (typeof json[project] !== "undefined" && typeof json[project][instance] !== "undefined") {
-         var alerts = json[project][instance].alerts || [];
-         var status = json[project][instance].status;
-
-         $(this).data("alerts", alerts);
-
-         var badge = null;
-         switch (status) {
-           case "planned":
-             badge = "badge-warning";
-             break;
-           case "inProgress":
-             badge = "badge-danger";
-             break;
-           case "finished":
-             badge = "badge-info";
-             break;
-           default:
-             badge = "badge-default";
-             break;
-         }
-
-         if (alerts.length > 0) {
-           $(td).html("<a href=\"#modal-alert\" data-toggle=\"modal\"><span class=\"badge badge-pill " + badge + "\">" + alerts.length + "</span></a>")
-         } else {
-           $(td).empty();
-         }
-       } else {
-         $(td).empty();
-       }
-     });
-   });
- }
-
-/* ************************************************************************
- *
- */
-function callBackCPU() {
-  $.getJSON("cloud-xhr.php", {
-    "cpu": 1,
-    "time": Date.now()
-  }, function(json) {
-    $("tr[data-instance]").each(function() {
-      var project = $(this).data("project");
-      var instance = $(this).data("instance");
-      var td = $(this).find("td.cpu-live");
-
-      if (typeof json[project][instance] === "object") {
-        $(td).html("<span title=\"" + Math.round(json[project][instance][0]) + " " + json[project][instance][1] + "\" class=\"" + (json[project][instance][2] < 50 ? "text-success" : (json[project][instance][2] < 75 ? "text-warning" : "text-danger")) + "\">" + json[project][instance][2] + "%</span>");
-        if (json[project][instance][3] === -1) {
-          $(td).append(" <i class=\"fa fa-angle-double-down fa-fw\" aria-hidden=\"true\"></i>");
-        } else if (json[project][instance][3] === 1) {
-          $(td).append(" <i class=\"fa fa-angle-double-up fa-fw\" aria-hidden=\"true\"></i>");
-        } else {
-          $(td).append(" <i class=\"fa fa-circle-thin fa-fw\" aria-hidden=\"true\" style=\"visibility: hidden;\"></i>");
-        }
-      } else {
-        $(td).html("<span class=\"text-muted\">N/A</span><a href=\"#console-" + consoleId + "\"><sup>" + consoleId + "</sup></a>");
-
-        $("#console > ol").append("<li id=\"console-" + consoleId + "\"><samp>" + json[project][instance] + "</samp></li>");
-        consoleId++;
-      }
-    });
-  });
-}
-
-/* ************************************************************************
- *
- */
-function callBackRAM() {
-  $.getJSON("cloud-xhr.php", {
-    "ram": 1,
-    "time": Date.now()
-  }, function(json) {
-    $("tr[data-instance]").each(function() {
-      var project = $(this).data("project");
-      var instance = $(this).data("instance");
-      var td = $(this).find("td.ram-live");
-
-      if (typeof json[project][instance] === "object") {
-        $(td).html("<span title=\"" + Math.round(json[project][instance][0]) + " " + json[project][instance][1] + "\" class=\"" + (json[project][instance][2] < 50 ? "text-success" : (json[project][instance][2] < 75 ? "text-warning" : "text-danger")) + "\">" + json[project][instance][2] + "%</span>");
-        if (json[project][instance][3] === -1) {
-          $(td).append(" <i class=\"fa fa-angle-double-down fa-fw\" aria-hidden=\"true\"></i>");
-        } else if (json[project][instance][3] === 1) {
-          $(td).append(" <i class=\"fa fa-angle-double-up fa-fw\" aria-hidden=\"true\"></i>");
-        } else {
-          $(td).append(" <i class=\"fa fa-circle-thin fa-fw\" aria-hidden=\"true\" style=\"visibility: hidden;\"></i>");
-        }
-      } else {
-        $(td).html("<span class=\"text-muted\">N/A</span><a href=\"#console-" + consoleId + "\"><sup>" + consoleId + "</sup></a>");
-
-        $("#console > ol").append("<li id=\"console-" + consoleId + "\"><samp>" + json[project][instance] + "</samp></li>");
-        consoleId++;
-      }
-    });
-  });
-}
